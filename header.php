66be93d6403141dddce29addb0c3deae6c72f8a7
Argit <stereofight91@gmail.com>
2019-04-04 09:37:09 +0300

    change styles

--- header.php
+++ header.php
@@ -1,49 +1,51 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title>Главная</title>
-	<base href="https://agrosouzplus.ru/" />
-			<meta name="viewport" content="width=992" />
-		<meta name="description" content="Агросоюз Плюс - это группа компаний, в лице централизированного отдела консультации потенциальных покупателей сельскохозяйственной техники от заводов-изготовителей состоящих в этой группе. Общаясь с представителем &quot;Агросоюз плюс&quot;, вы общаетесь с поставщиком напрямую. Никаких посреднических процентов, никаких посреднических договоров. У нас Вы найдете такое оборудование, как: зерноочистительная техника, нории, зерносушилки, прицепные опрыскиватели, строительство зерновых комплексов и т.д." />
-	<meta name="keywords" content="агросоюз плюс,зернопогрузочная техника,складская сельхозтехника,агрострой,воронежская сельхозтехника" />
-	<!-- Facebook Open Graph -->
-	<meta property="og:title" content="Главная" />
-	<meta property="og:description" content="Агросоюз Плюс - это группа компаний, в лице централизированного отдела консультации потенциальных покупателей сельскохозяйственной техники от заводов-изготовителей состоящих в этой группе. Общаясь с представителем &quot;Агросоюз плюс&quot;, вы общаетесь с поставщиком напрямую. Никаких посреднических процентов, никаких посреднических договоров. У нас Вы найдете такое оборудование, как: зерноочистительная техника, нории, зерносушилки, прицепные опрыскиватели, строительство зерновых комплексов и т.д." />
-	<meta property="og:image" content="https://agrosouzplus.ru/gallery_gen/1f830483bb3ebb371cdee1f66ecf4a5e.png" />
-	<meta property="og:type" content="article" />
-	<meta property="og:url" content="https://agrosouzplus.ru/" />
-	<!-- Facebook Open Graph end -->
-		
-	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
-	<script src="js/bootstrap.min.js" type="text/javascript"></script>
-	<script src="js/main.js?v=20190227101223" type="text/javascript"></script>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Главная</title>
+    <base href="https://agrosouzplus.ru/" />
+    <meta name="viewport" content="width=992" />
+    <meta name="description" content="Агросоюз Плюс - это группа компаний, в лице централизированного отдела консультации потенциальных покупателей сельскохозяйственной техники от заводов-изготовителей состоящих в этой группе. Общаясь с представителем &quot;Агросоюз плюс&quot;, вы общаетесь с поставщиком напрямую. Никаких посреднических процентов, никаких посреднических договоров. У нас Вы найдете такое оборудование, как: зерноочистительная техника, нории, зерносушилки, прицепные опрыскиватели, строительство зерновых комплексов и т.д." />
+    <meta name="keywords" content="агросоюз плюс,зернопогрузочная техника,складская сельхозтехника,агрострой,воронежская сельхозтехника" />
+    <!-- Facebook Open Graph -->
+    <meta property="og:title" content="Главная" />
+    <meta property="og:description" content="Агросоюз Плюс - это группа компаний, в лице централизированного отдела консультации потенциальных покупателей сельскохозяйственной техники от заводов-изготовителей состоящих в этой группе. Общаясь с представителем &quot;Агросоюз плюс&quot;, вы общаетесь с поставщиком напрямую. Никаких посреднических процентов, никаких посреднических договоров. У нас Вы найдете такое оборудование, как: зерноочистительная техника, нории, зерносушилки, прицепные опрыскиватели, строительство зерновых комплексов и т.д." />
+    <meta property="og:image" content="https://agrosouzplus.ru/gallery_gen/1f830483bb3ebb371cdee1f66ecf4a5e.png" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="https://agrosouzplus.ru/" />
+    <!-- Facebook Open Graph end -->
 
-	<link href="css/font-awesome/font-awesome.min.css?v=4.7.0" rel="stylesheet" type="text/css" />
-	<link href="css/site.css?v=20190227101229" rel="stylesheet" type="text/css" />
-	<link href="css/common.css?ts=1551688539" rel="stylesheet" type="text/css" />
-	<link href="css/1.css?ts=1551688539" rel="stylesheet" type="text/css" />
-	<link rel="icon" href="/gallery/агросоюз логотип-ts1545313026.png" type="image/png" />
-	<script type="text/javascript">
-	window.useTrailingSlashes = true;
-</script>
-	
-	<link href="css/flag-icon-css/css/flag-icon.min.css" rel="stylesheet" type="text/css" />	
-	<!--[if lt IE 9]>
-	<script src="js/html5shiv.min.js"></script>
-	<![endif]-->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="js/main.js?v=20190227101223" type="text/javascript"></script>
+
+    <link href="css/font-awesome/font-awesome.min.css?v=4.7.0" rel="stylesheet" type="text/css" />
+    <link href="css/site.css?v=20190227101229" rel="stylesheet" type="text/css" />
+    <link href="css/common.css?ts=1551688539" rel="stylesheet" type="text/css" />
+    <link href="css/1.css?ts=1551688539" rel="stylesheet" type="text/css" />
+    <link rel="icon" href="/gallery/агросоюз логотип-ts1545313026.png" type="image/png" />
+    <script type="text/javascript">
+        window.useTrailingSlashes = true;
+    </script>
+
+    <link href="css/flag-icon-css/css/flag-icon.min.css" rel="stylesheet" type="text/css" />
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+    <![endif]-->
     <?php wp_head(); ?>
-	</head>
+</head>
 
-<body><div class="root"><div class="vbox wb_container" id="wb_header">
 
-        <div class="wb_cont_inner"><div id="wb_element_instance149" class="wb_element wb_text_element" style=" line-height: normal;"><h1 class="wb-stl-heading1"><strong><font color="#ffffff">8(918) 932-32-47</font></strong></h1>
-            </div><div id="wb_element_instance150" class="wb_element wb_element_picture" title=""><a href=""><img alt="gallery/агросоюз логотип" src="gallery_gen/1f830483bb3ebb371cdee1f66ecf4a5e_173x140.png"></a></div><div id="wb_element_instance151" class="wb_element"><a class="wb_button" href=""><span>Главная</span></a></div><div id="wb_element_instance152" class="wb_element"><a class="wb_button" href="contacts/"><span>Контакты</span></a></div><div id="wb_element_instance153" class="wb_element"><a class="wb_button" href="%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B8/"><span>Новости</span></a></div><div id="wb_element_instance154" class="wb_element" style=" overflow: hidden;"><div style="height: 100%;"><!-- BEGIN JIVOSITE CODE {literal} -->
+<body>
+<div class="root"><div class="vbox wb_container" id="wb_header">
+
+        <div class="wb_cont_inner"><div id="wb_element_instance0" class="wb_element wb_text_element" style=" line-height: normal;"><h1 class="wb-stl-heading1"><strong><font color="#ffffff">8(918) 932-32-47</font></strong></h1>
+            </div><div id="wb_element_instance1" class="wb_element wb_element_picture" title=""><a href=""><img alt="gallery/агросоюз логотип" src="gallery_gen/1f830483bb3ebb371cdee1f66ecf4a5e_173x140.png"></a></div><div id="wb_element_instance2" class="wb_element"><a class="wb_button" href=""><span>Главная</span></a></div><div id="wb_element_instance3" class="wb_element"><a class="wb_button" href="contacts/"><span>Контакты</span></a></div><div id="wb_element_instance4" class="wb_element"><a class="wb_button" href="%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B8/"><span>Новости</span></a></div><div id="wb_element_instance5" class="wb_element" style=" overflow: hidden;"><div style="height: 100%;"><!-- BEGIN JIVOSITE CODE {literal} -->
                     <script type="text/javascript">
                         (function(){ var widget_id = 'w0mVU4n6kf';var d=document;var w=window;function l(){var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-                    </script><!-- {/literal} END JIVOSITE CODE --></div></div><div id="wb_element_instance155" class="wb_element wb_text_element" style=" line-height: normal;"><h3 class="wb-stl-heading3"><strong><font color="#ffffff">Пн-Пт 8:00-18:00</font></strong></h3>
-            </div><div id="wb_element_instance156" class="wb_element" style=" overflow: hidden;"><div style="height: 100%;"><!-- Yandex.Metrika counter -->
+                    </script><!-- {/literal} END JIVOSITE CODE --></div></div><div id="wb_element_instance6" class="wb_element wb_text_element" style=" line-height: normal;"><h3 class="wb-stl-heading3"><strong><font color="#ffffff">Пн-Пт 8:00-18:00</font></strong></h3>
+            </div><div id="wb_element_instance7" class="wb_element" style=" overflow: hidden;"><div style="height: 100%;"><!-- Yandex.Metrika counter -->
                     <script type="text/javascript">
                         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                             m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -57,19 +59,11 @@
                             webvisor:true
                         });
                     </script><noscript><div><img src="https://mc.yandex.ru/watch/52253167" style="position:absolute; left:-9999px;" alt=""></div></noscript>
-                    <!-- /Yandex.Metrika counter --></div></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+                    <!-- /Yandex.Metrika counter --></div></div>
+        </div>
+        <div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
     <div class="vbox wb_container" id="wb_main">
 
-
-    <?php
-    /*wp_nav_menu(array(
-        'menu' => 'Primary',
-        'theme_location' => 'primary',
-        'container' => false,
-        'menu_class' => 'hmenu',
-        'depth' => 0
-    ));*/
-    ?>
-    <!-- <div class="clearfix"></div> -->
-    <!-- </div> -->
-
+        <div class="wb_cont_inner">
+            <div id="wb_element_instance8" class="wb_element wb-menu">
+            <ul class="hmenu"><li><a href="zernoochistka/" target="_self">Зерноочистительное оборудование</a><ul><li><a href="predvaritelnaya_ochistka/" target="_self">Предварительная очистка</a><ul><li><a href="MPO50/" target="_self">МПО-50</a></li><li><a href="MZK-70/" target="_self">МЗК-70</a></li></ul></li><li><a href="pervichnaya_ochistka/" target="_self">Первичная очистка</a><ul><li><a href="ZVS/" target="_self">ЗВС-20</a></li><li><a href="OVS/" target="_self">ОВС-25</a></li><li><a href="VRM/" target="_self">ВРМ-К 52.7</a></li><li><a href="ZSK-70/" target="_self">ЗСК-70</a></li></ul></li><li><a href="vtorichnaya_ochistka/" target="_self">Вторичная очистка</a><ul><li><a href="trier/" target="_self">Триерный блок АЗТБ</a></li></ul></li></ul></li><li class="active"><a href="transport/" target="_self">Транспорт</a><ul><li><a href="norii/" target="_self">Нории</a></li><li><a href="transporter_tsc/" target="_self">Транспортер горизонтальный ТСЦ</a></li><li><a href="zernometateli/" target="_self">Зернометатели</a></li></ul></li><li><a href="zernosyshilki/" target="_self">Зерносушилки</a><ul><li><a href="ventum_syshilka/" target="_self">Шахтные сушилки Ventum</a></li><li><a href="zernosyshilki_atm/" target="_self">Мобильные зерносушилки АТМ</a></li></ul></li><li><a href="ZAV/" target="_self">Зерновые комплексы</a></li><li><a href="opriskivateli/" target="_self">Опрыскиватели</a></li></ul><div class="clearfix"></div></div>
