a64eaa9ffba0f753728596dbed4cac8f0a584b4b
Argit <stereofight91@gmail.com>
2019-04-06 12:17:57 +0300

    block

--- page.php
+++ page.php
@@ -3,14 +3,14 @@
 <div class="middle">
 
     <div class="container">
-        <main class="content">
-        <?php the_post();?>
-        <?php the_content();?>
+        <div class="content">
+            <?php the_post();?>
+            <?php the_content();?>
+           </div><!-- .content-->
     </div><!-- .container-->
 
-    <aside class="left-sidebar">
-        <strong>Left Sidebar:</strong> Integer velit. Vestibulum nisi nunc, accumsan ut, vehicula sit amet, porta a, mi. Nam nisl tellus, placerat eget, posuere eget, egestas eget, dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In elementum urna a eros. Integer iaculis. Maecenas vel elit.
-        <a href="#" class="expand">Expand column</a>		</aside><!-- .left-sidebar -->
+    <div class="left-sidebar">
+    </div><!-- .left-sidebar -->
 
 </div><!-- .middle-->
 
